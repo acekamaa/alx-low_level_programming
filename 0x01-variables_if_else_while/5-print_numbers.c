ce741f2cb51b9fa2f56138ed7a7ff3faae7799c0
acekamaa <kvnkamaa@gmail.com>
2023-02-20 17:54:04 -0800

    base 10num

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,11 @@
 /* program that prints digits of base ten */
 #include <stdio.h>
-int main() {
+int main(void)
+{
 	/* declare variable */
 	int i;
-	for (i=0 ;i < 10 ; i++) {
-		putchar(i);
-	}
-	return 0;
+
+	for (i = 0 ;i < 10 ; i++)
+		putchar(i); 
+	
 }
