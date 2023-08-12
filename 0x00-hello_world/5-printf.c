e665020df596fee1ce418cb19233e3015ea95f85
acekamaa <kvnkamaa@gmail.com>
2023-08-11 20:56:13 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+ * main is entry point
+ * use printf
+ * return 0 if success
+ */
+int main(void) 
+{
+	printf("with proper grammar, but the outcome is a piec of art");
+	return(0);
+}
