12ae34686177fc7fc9a9466838fda12a9e92c489
acekamaa <kvnkamaa@gmail.com>
2023-08-12 05:08:16 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-/*
+/**
  * main is entry
  *
  * return 0 if success
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle);
 		return (0);
 }
