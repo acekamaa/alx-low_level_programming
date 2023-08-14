e820c0e2d30a90539b46225649c8286428fd7d39
acekamaa <kvnkamaa@gmail.com>
2023-08-14 06:07:59 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point, prints single numbers of base 10 from 0
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
