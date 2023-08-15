97cbc9bdb69b1b9c697c17e1aa51732682ac8475
acekamaa <kvnkamaa@gmail.com>
2023-08-15 12:26:33 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+0x02. C - Functions, nested loops
+walk through loops and functions in c
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h
+#define main_h
+
+/* holds prototype of my project on functions and nested loops */
+/*  _putchar prints to stdout */
+int _putchar(char)
