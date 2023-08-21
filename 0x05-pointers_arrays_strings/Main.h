bda69ed57f149ddd2ff679ac222a5d7b08186671
acekamaa <kvnkamaa@gmail.com>
2023-08-21 17:49:48 +0300

    1. Don't swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/Main.h
+++ 0x05-pointers_arrays_strings/Main.h
@@ -6,5 +6,6 @@
  * stores prototypes to this project
  */
 void reset_to_98(int *n); /* n here is a pointer to a parameter */
+void swap_int(int *a, int *b); /* @a and @b are swap integer parameters */
 
 #endif
