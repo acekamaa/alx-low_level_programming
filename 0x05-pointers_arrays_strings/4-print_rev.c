97a107031a68ea28160926e07882c7e1177c1251
acekamaa <kvnkamaa@gmail.com>
2023-08-21 23:46:43 +0300

    4. I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "Main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @str: holds the value of string
+ */
+void print_rev(char *str)
+{
+	int y = 0;
+
+	while (*str != '\0')
+	{
+		y++;
+		str++;
+	}
+}
--- 0x05-pointers_arrays_strings/Main.h
+++ 0x05-pointers_arrays_strings/Main.h
@@ -10,5 +10,6 @@ void swap_int(int *a, int *b); /* @a and @b are swap integer parameters */
 int _strlen(char *s); /* @s returns the length of a string */
 int _strlen(char *s); /* @s returns size of the string */
 void _puts(char *str); /* @str prints a string */
+void print_rev(char *s); /* @s prints a string in reverse */
 
 #endif
