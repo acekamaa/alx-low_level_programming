f823ae7d0b762df9671d238040d3cdea3bd02925
acekamaa <kvnkamaa@gmail.com>
2023-08-24 20:51:52 +0300

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copies a string
+ * @dest: holds first char
+ * @src: holds const char
+ * Return: Always orig(success)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *orig = dest;
+
+	while (n > 0 && *src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	while (n > 0)
+	{
+		*dest = '\0';
+		dest++;
+		n--;
+	}
+	return (orig);
+}
