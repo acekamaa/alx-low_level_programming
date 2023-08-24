815fb7b6f2276862bdc97308d4682a0574148bf9
acekamaa <kvnkamaa@gmail.com>
2023-08-24 23:38:19 +0300

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: holds first string
+ * @s2: holds second string
+ * Return: Always 1(success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 < *s2)
+		{
+			return (-1);
+		}
+		else if (*s1 > *s2)
+		{
+			return (1);
+		}
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (0);
+	}
+	else if (*s1 == '\0')
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+}
