fc08254aa1f3f6a89f5b48132808cbe4132b9372
acekamaa <kvnkamaa@gmail.com>
2023-08-30 23:11:33 +0300

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: holds a char
+ *
+ * Return: Nothing.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\0');
+		return;
+	}
+
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
