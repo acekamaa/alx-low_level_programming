c669129d8b0c750f44d4302e87b175a16c87a73a
= <=>
2024-02-08 18:56:05 +0300

    correct code for quiz 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,17 +11,12 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int divisor, check;
+	unsigned long int setbit;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
-	{
 		return (-1);
-	}
-	divisor = 1 << index;
-	check = *n & index;
-	if (check == divisor)
-	{
-		return (1);
-	}
-	return (0);
+
+	setbit = 1 << index;
+	*n = *n | setbit;
+	return (1);
 }
